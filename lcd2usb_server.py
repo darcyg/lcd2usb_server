4c7e8790c9e4322389d5cb3f52420472e18990c0
Jacob Stewart <stewart.w.jacob@gmail.com>
2018-03-30 14:42:02 -0400

    Moved LCD line variables into lcd_thread from global.

--- lcd2usb_server.py
+++ lcd2usb_server.py
@@ -12,14 +12,51 @@ verbose = False  #"-v"
 allowExternal = False #"-e"
 
 # Original Line Variables
-line_0 = "%I:%M%p"
-line_1 = " "
-line_2 = "Waiting for"
-line_3 = "connection..."
+line0_default = "%I:%M%p"
+line1_default = " "
+line2_default = "Waiting for"
+line3_default = "connection..."
 
 # Socket Default values
 portNum = 8080
 server_address = ('localhost', portNum)
+
+class lcd_thread(threading.Thread):
+  def __init__(self):
+    threading.Thread.__init__(self)
+    lcd = LCD.find_or_die()
+    line0 = line0_default
+    line1 = line1_default
+    line2 = line2_default
+    line3 = line3_default
+  def run(self):
+    self.lcd_refresh_loop()
+  def lcd_refresh(self):
+    import datetime
+    dt = datetime.datetime.now()
+    lcd.home()
+    lcd.fill_center(dt.strftime(line_0),0)
+    lcd.fill_center(dt.strftime(line_1),1)
+    lcd.fill_center(dt.strftime(line_2),2)
+    lcd.fill_center(dt.strftime(line_3),3)
+  def lcd_refresh_loop(self):
+    while True:
+      lcd_refresh()
+      time.sleep(2)   
+  def set_line0(new_string):
+    line0 = new_string
+    lcd_refresh()
+  def set_line1(new_string):
+    line1 = new_string
+    lcd_refresh()
+  def set_line2(new_string):
+    line2 = new_string
+    lcd_refresh()
+  def set_line3(new_string):
+    line3 = new_string
+    lcd_refresh()
+  def set_brightness(brightness):
+    lcd.set_brightness(brightness)
 
 def input_handler(ctl_data, str_data):
   global line_0, line_1, line_2, line_3, lcd
@@ -27,46 +64,29 @@ def input_handler(ctl_data, str_data):
     if (verbose):
       print "Setting line 0 to: ", str_data
     if (not noDisplay):
-      line_0 = str_data
+      lcd_thread.set_line0(str_data)
   elif ctl_data == "1":
     if (verbose):
       print "Setting line 1 to: ", str_data
     if (not noDisplay):
-      line_1 = str_data
+      lcd_thread.set_line1(str_data)
   elif ctl_data == "2":
     if (verbose):
       print "Setting line 2 to: ", str_data
     if (not noDisplay):
-      line_2 = str_data
+      lcd_thread.set_line2(str_data)
   elif ctl_data == "3":
     if (verbose):
       print "Setting line 3 to: ", str_data
     if (not noDisplay):
-      line_3 = str_data
+      lcd_thread.set_line3(str_data)
   elif ctl_data == "@":
     if (verbose):
       print "Setting brightness to: ", str_data
     if (not noDisplay):
-      lcd.set_brightness(int(str_data))
+      lcd_thread.set_brightness(int(str_data))
   else:
     print "Bad ctl_data value"
-
-
-class lcd_thread(threading.Thread):
-  def __init__(self):
-    lcd = LCD.find_or_die()
-  def run(self):
-    self.lcd_refresh_loop()
-  def lcd_refresh_loop(self):
-    while True:
-      import datetime
-      dt = datetime.datetime.now()
-      lcd.home()
-      lcd.fill_center(dt.strftime(line_0),0)
-      lcd.fill_center(dt.strftime(line_1),1)
-      lcd.fill_center(dt.strftime(line_2),2)
-      lcd.fill_center(dt.strftime(line_3),3)
-      time.sleep(2)    
 
 if (__name__ == "__main__"):
 
@@ -84,9 +104,8 @@ if (__name__ == "__main__"):
       print "Invalid Argument"
       exit()
     
-
   if (not noDisplay):
-    lcd_update = lcd_thread(name = "lcd_update_thread")
+    lcd_update = lcd_thread()
     lcd_update.start()
 
   serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
